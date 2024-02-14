7ff70417ba8aeb7dc37c9e6858802dc9e1a7ea94
Avi Ab <aviavia4u@gmail.com>
2024-02-14 23:24:36 +0200

    added snipcart api key dynamically

--- src/_data/snipcart.js
+++ src/_data/snipcart.js
@@ -1,7 +1,13 @@
-const yaml = require('js-yaml');
 const fs = require('fs');
+const yaml = require('js-yaml');
 
 module.exports = () => {
-  const doc = yaml.load(fs.readFileSync('snipcartSettings.yaml', 'utf8'));
-  return doc;
+  try {
+    // If snipcart.js and snipcartSettings.yaml are in the same directory
+    const doc = yaml.load(fs.readFileSync('snipcartSettings.yaml', 'utf8'));
+    return doc;
+  } catch (e) {
+    console.log(e);
+    return {}; // Return an empty object in case of an error
+  }
 };
